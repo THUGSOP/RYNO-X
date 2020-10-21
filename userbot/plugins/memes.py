b8652918a3bbc9e1f2f0a48c1bbea6ee31ce6872
rhino-x <73199029+rhinox-rgb@users.noreply.github.com>
2020-10-21 21:14:03 +0530

    Update memes.py

--- userbot/plugins/memes.py
+++ userbot/plugins/memes.py
@@ -850,7 +850,7 @@ async def vapor(vpr):
 @register(outgoing=True, pattern="^.repo$")
 async def source(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        await e.edit("Click [here](https://github.com/HellBoy-OP/HellBot) to open this 🔥**Lit AF!!**🔥 __Hêllẞø†__ Repo.. Join channel :- @HellBot_Official")
+        await e.edit("Click [here](https://github.com/rhinox-rgb/RYNOX) to open this 🔥**Lit AF!!**🔥 __Hêllẞø†__ Repo.. Join channel :- @OFFICIALRYNOX")
 			  
 			  
 @register(outgoing=True, pattern="^.str(?: |$)(.*)")
