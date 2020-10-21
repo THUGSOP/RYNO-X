28404ff28b5efe63965a2fe9e00121f1b9be2d63
rhino-x <73199029+rhinox-rgb@users.noreply.github.com>
2020-10-21 20:44:33 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -3,25 +3,25 @@ from telethon import events
 from userbot.utils import admin_cmd
 from userbot import ALIVE_NAME
 from telethon.tl.types import ChannelParticipantsAdmins
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Hell User"
-PM_IMG = "https://telegra.ph/file/f34675b4e94d4290c0b6b.mp4"
-pm_caption = "🔥🔥**Hêllẞø† IS ONLINE🔥🔥\n\n\n"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "RYNO-X User"
+PM_IMG = "https://telegra.ph/file/04a43e5aa4ec612b92af6.mp4"
+pm_caption = "🔥🔥**RYNO-X IS ONLINE🔥🔥\n\n\n"
 
 pm_caption += f"⚔️⚔️**MASTER**⚔️⚔️       : {DEFAULTUSER}\n\n"
 
 pm_caption += "🛡️🛡️**TELETHON**🛡️🛡️   : 1.15.0 \n\n"
 
-pm_caption += "😈😈**Hêllẞø†**😈😈         : __V-1.1__\n\n"
+pm_caption += "😈😈**RYNO-X**😈😈         : __V-1.1__\n\n"
 
-pm_caption += "⚠️⚠️**CHANNEL**⚠️⚠️     : [ᴊᴏɪɴ](https://t.me/HellBot_Official)\n\n"
+pm_caption += "⚠️⚠️**CHANNEL**⚠️⚠️     : [ᴊᴏɪɴ](https://t.me/OFFICIALRYNOX)\n\n"
 
-pm_caption += "🔱🔱**GROUP**🔱🔱.         : [ᴊᴏɪɴ](https://t.me/HellBot_Official_Chat)\n\n"
+pm_caption += "🔱🔱**GROUP**🔱🔱.         : [ᴊᴏɪɴ](https://t.me/RYNOXCHAT)\n\n"
 
-pm_caption += "😎😎**LICENSE**😎😎       : [ӀíϲҽղՏҽ](https://github.com/HellBoy-OP/HellBot/blob/master/LICENSE)\n\n"
+pm_caption += "😎😎**LICENSE**😎😎       : [ӀíϲҽղՏҽ](https://github.com/rhinox-rgb/RYNOX/blob/master/LICENSE)\n\n"
 
-pm_caption += "🔥🔥**CREATOR🔥🔥      : [HellBot-Owner](https://t.me/kraken_the_badass)\n\n"
+pm_caption += "🔥🔥**CREATOR🔥🔥      : [HellBot-Owner](https://t.me/HUNTER_YUVRAJ)\n\n"
 
-pm_caption += " [...▄███▄███▄\n....█████████\n.......▀█████▀\n............▀█▀\n](https://t.me/hellbot_official)\n\n"
+pm_caption += " [...▄███▄███▄\n....█████████\n.......▀█████▀\n............▀█▀\n](https://t.me/OFFICIALRYNOX)\n\n"
 #@command(outgoing=True, pattern="^.alive$")
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
