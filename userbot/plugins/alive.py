a53c54cfb3f0c15506c4765e1ea2ca2f76049536
RYNO-X <73129419+RYNO-X@users.noreply.github.com>
2020-11-23 08:06:27 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,7 +1,15 @@
 import asyncio
+
 from telethon import events
-from userbot.utils import admin_cmd
+
+from userbot.utils import admin_cmd, sudo_cmd
+
 from userbot import ALIVE_NAME
+
+from telethon.tl.types import ChannelParticipantsAdmins
+
+
+
 from telethon.tl.types import ChannelParticipantsAdmins
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "RYNO-X User"
 PM_IMG = "https://telegra.ph/file/04a43e5aa4ec612b92af6.mp4"
@@ -11,7 +19,7 @@ pm_caption += f"⚔️⚔️**MASTER**⚔️⚔️       : {DEFAULTUSER}\n\n"
 
 pm_caption += "🛡️🛡️**TELETHON**🛡️🛡️   : 1.02.0 \n\n"
 
-pm_caption += "😈😈**RYNO-X**😈😈         : __V-1.1__\n\n"
+pm_caption += "😈😈**RYNO-X**😈😈         : __V-1.4__\n\n"
 
 pm_caption += "⚠️⚠️**CHANNEL**⚠️⚠️     : [ᴊᴏɪɴ](https://t.me/OFFICIALRYNOX)\n\n"
 
@@ -23,10 +31,22 @@ pm_caption += "🔥🔥**CREATOR🔥🔥      : [RYNO-X OWNER](https://t.me/HUNT
 
 pm_caption += " [...▄███▄███▄\n....█████████\n.......▀█████▀\n............▀█▀\n](https://t.me/OFFICIALRYNOX)\n\n"
 #@command(outgoing=True, pattern="^.alive$")
-@borg.on(admin_cmd(pattern=r"alive"))
+@bot.on(admin_cmd(outgoing=True, pattern="alive$"))
+
+@bot.on(sudo_cmd(pattern="alive$", allow_sudo=True))
+
 async def amireallyalive(alive):
+
     chat = await alive.get_chat()
+
     await alive.delete()
+
     """ For .alive command, check if the bot is running.  """
+
     await borg.send_file(alive.chat_id, PM_IMG,caption=pm_caption)
+
     await alive.delete() 
+
+
+
+
