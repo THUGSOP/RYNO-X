e7d55045bde01fd59f910afb3b6825e32545c319
RYNO-X <73129419+RYNO-X@users.noreply.github.com>
2020-11-28 11:17:52 +0530

    Update uniborgConfig.py

--- userbot/uniborgConfig.py
+++ userbot/uniborgConfig.py
@@ -136,6 +136,9 @@ if ENV:
             "UPSTREAM_REPO", "https://github.com/RYNO-X/RYNO-X"
         )
         PM_DATA = os.environ.get("PM_DATA", "ENABLE")
+        # Deepai value can get from https://deepai.org/
+
+        DEEP_AI = os.environ.get("DEEP_AI", None)
         
 else:
     class Config(object):
